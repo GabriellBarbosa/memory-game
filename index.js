2fd830a7bf10cc64ce031829a741fc611edfa773
Gabriel Barbosa <bielbarbosa900@gmail.com>
2021-10-20 22:09:14 -0300

    tela de vire seu dispositivo criada

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import { Cards } from './js/cards.js';
+import { debounce } from './js/debounce.js';
+
+// Tela de aviso: virar o celular
+  const turnPhoneElement = document.querySelector('.turn-phone-container');
+  if(window.innerWidth < 380) { 
+    turnPhoneElement.classList.add('active');
+  }
+  const handleScreenWidth = () => {
+    if(window.innerWidth > 380) {
+      turnPhoneElement.classList.remove('active');
+    } else {
+      turnPhoneElement.classList.add('active');
+    }
+  }
+  window.addEventListener('resize', debounce(handleScreenWidth, 200));
 
 const cards = new Cards('.memory_card');
 cards.execute();
