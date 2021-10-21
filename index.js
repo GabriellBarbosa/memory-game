61d6e4a7b33668e8e7973d0489b13d11bc9dafaa
Gabriel Barbosa <bielbarbosa900@gmail.com>
2021-10-20 22:20:18 -0300

    largura miníma da aumentada para 440

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import { debounce } from './js/debounce.js';
 
 // Tela de aviso: virar o celular
   const turnPhoneElement = document.querySelector('.turn-phone-container');
-  if(window.innerWidth < 380) { 
+  if(window.innerWidth < 440) { 
     turnPhoneElement.classList.add('active');
   }
   const handleScreenWidth = () => {
-    if(window.innerWidth > 380) {
+    if(window.innerWidth > 440) {
       turnPhoneElement.classList.remove('active');
     } else {
       turnPhoneElement.classList.add('active');
