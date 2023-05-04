63dd1322925d147dd655b47fd406cb1ed9235ad1
GabriellBarbosa <bielbarbosa900@gmail.com>
2023-05-04 16:57:58 -0300

    cards match

--- js/cards.test.js
+++ js/cards.test.js
@@ -4,6 +4,8 @@
 
 import { Cards } from "./cards";
 
+jest.useFakeTimers();
+
 let spy;
 beforeAll(() => {
     spy = jest.spyOn(document, 'querySelectorAll');
@@ -11,24 +13,24 @@ beforeAll(() => {
 
 describe('Cards class', () => {
     let memoryGame;
-    beforeAll(() => {
+    beforeEach(() => {
         const mockElement = document.createElement('section');
         mockElement.innerHTML = 
         `
-            <div class="modal-bg">
-                <div class="modal">
-                <h2>Título</h2>
-                <p>Mensagem</p>
-                <span>Recorde</span>
-                <button>Jogar novamente</button>
-                </div>
+        <div class="modal-bg">
+            <div class="modal">
+            <h2>Título</h2>
+            <p>Mensagem</p>
+            <span>Recorde</span>
+            <button>Jogar novamente</button>
             </div>
+        </div>
 
-            <div class="memory_card" data-card="spider"></div>
-            <div class="memory_card" data-card="spider"></div>
-        
-            <div class="memory_card" data-card="hulk"></div>
-            <div class="memory_card" data-card="hulk"></div>
+        <div class="memory_card" data-card="spider"></div>
+        <div class="memory_card" data-card="spider"></div>
+    
+        <div class="memory_card" data-card="hulk"></div>
+        <div class="memory_card" data-card="hulk"></div>
         `;
         spy.mockReturnValue(mockElement);
         const cardsNodeList = mockElement.querySelectorAll('.memory_card');
@@ -36,7 +38,7 @@ describe('Cards class', () => {
         memoryGame = new Cards(cardsNodeList, modalElement);
         const initMemoryGame = jest.fn(memoryGame.execute);
         initMemoryGame();
-    });
+    })
 
     it('should have four cards in this test', () => {
         expect(Array.from(memoryGame.cards).length).toBe(4);
@@ -67,5 +69,31 @@ describe('Cards class', () => {
         thirdCard.click();
 
         expect(Array.from(thirdCard.classList)).not.toContain('flip');
+    });
+
+    it('should flip cards back after 1.2sec if not match', () => {
+        const spiderCard = memoryGame.cards[0];
+        const hulkCard = memoryGame.cards[3];
+
+        spiderCard.click(); 
+        hulkCard.click();
+
+        jest.advanceTimersByTime(1200);
+
+        expect(Array.from(spiderCard.classList)).not.toContain('flip');
+        expect(Array.from(hulkCard.classList)).not.toContain('flip');
+    });
+
+    it('should keep cards flipped after 1.2sec if match', () => {
+        const spiderCard1 = memoryGame.cards[0];
+        const spiderCard2 = memoryGame.cards[1];
+
+        spiderCard1.click();
+        spiderCard2.click();
+
+        jest.advanceTimersByTime(1200);
+
+        expect(Array.from(spiderCard1.classList)).toContain('flip');
+        expect(Array.from(spiderCard2.classList)).toContain('flip');
     });
 });
