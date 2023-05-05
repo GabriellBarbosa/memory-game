438b0fc50f91d9a49cbd302d4772eda58f3da23a
GabriellBarbosa <bielbarbosa900@gmail.com>
2023-05-05 10:54:35 -0300

    call init method correctly

--- js/cards.test.js
+++ js/cards.test.js
@@ -43,8 +43,7 @@ describe('Cards class', () => {
         cards = mockElement.querySelectorAll('.memory_card');
         modal = mockElement.querySelector('.modal-bg')
         memoryGame = new Cards(cards, modal);
-        const initMemoryGame = jest.fn(memoryGame.init);
-        initMemoryGame();
+        jest.fn(memoryGame.init());
     });
 
     it('should have four cards', () => {
