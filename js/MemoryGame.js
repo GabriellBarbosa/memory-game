d407df367aee886568977ce76a5c65829d06fe83
GabriellBarbosa <bielbarbosa900@gmail.com>
2023-05-08 11:08:36 -0300

    move functions

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -15,7 +15,7 @@ class MemoryGame {
   }
 
   _shuffleCards() {
-    this._cards.forEach(card => {
+    this._cards.forEach((card) => {
       const randomNumber = Math.ceil(Math.random() * 15);
       card.style.order = String(randomNumber);
     });
@@ -23,13 +23,13 @@ class MemoryGame {
 
   _flipCardOnClick() {
     this._cards.forEach((card) => {
-      card.addEventListener('click', this.flip);
+      card.addEventListener("click", this.flip);
     });
   }
 
   addEvents() {
-    const restartButton = this._modal.querySelector('button');
-    restartButton.addEventListener('click', this.restart);
+    const restartButton = this._modal.querySelector("button");
+    restartButton.addEventListener("click", this.restart);
   }
 
   bindEvents() {
@@ -43,81 +43,18 @@ class MemoryGame {
   };
 
   addClickEvent = (element) => {
-    element.addEventListener('click', this.flip);
+    element.addEventListener("click", this.flip);
     return element;
   };
 
   removeClickEvent = (element) => {
-    element.removeEventListener('click', this.flip);
+    element.removeEventListener("click", this.flip);
     return element;
-  };
-
-  // desvira as cartas se não forem iguais
-  noMatch = () => {
-    setTimeout(() => {
-      this.addClickEvent(this._firstCard);
-      this.addClickEvent(this._secondCard);
-      this._firstCard.classList.remove('flip');
-      this._secondCard.classList.remove('flip');
-      this.reset();
-    }, 1200);
-  };
-
-  // mantém as cartas viradas se forem iguais
-  match = () => {
-    this.removeClickEvent(this._firstCard);
-    this.removeClickEvent(this._secondCard);
-    this.reset();
-  };
-
-  gameOverMessages = () => {
-    const modalElements = {
-      title: this._modal.querySelector('h2'),
-      message: this._modal.querySelector('p'),
-      record: this._modal.querySelector('span')
-    }
-
-    const record = window.localStorage.getItem('record');
-    if(!record || this.userAttempts < record) {
-      window.localStorage.setItem('record', this.userAttempts);
-      modalElements.title.innerText = 'Parabéns, você bateu o seu recorde!!';
-      modalElements.record.innerText = `Recorde: ${this.userAttempts}`;
-    } else {
-      modalElements.title.innerText = 'Game Over, tente novamente!';
-      modalElements.record.innerText = `Recorde: ${record}`;
-    }
-
-    modalElements.message.innerText = `${this.userAttempts} tentativas`;
-  }
-
-  gameOver = () => {
-    const flipped = this._cards.filter((card) => {
-      return card.classList.contains('flip');
-    });
-
-    if(flipped.length === this._cards.length) {
-      this.gameOverMessages();
-      this._modal.classList.add('active');
-    }
-  }
-
-  // verifica se o dataset são iguais
-  handleCardsMatch = () => {
-    if (this._firstCard && this._secondCard) {
-      this.userAttempts += 1;
-      const datasetMatch = this._firstCard.dataset.card === this._secondCard.dataset.card;
-      if (datasetMatch) {
-        this.match();
-        this.gameOver();
-      } else {
-        this.noMatch();
-      }
-    }
   };
 
   flip(card) {
     if (!this._firstCard || !this._secondCard) {
-      card.currentTarget.classList.toggle('flip');
+      card.currentTarget.classList.toggle("flip");
       this.checkCards(card.currentTarget);
     }
   }
@@ -134,15 +71,78 @@ class MemoryGame {
     this.handleCardsMatch();
   };
 
+  handleCardsMatch = () => {
+    if (this._firstCard && this._secondCard) {
+      this.userAttempts += 1;
+      const datasetMatch =
+        this._firstCard.dataset.card === this._secondCard.dataset.card;
+      if (datasetMatch) {
+        this.match();
+        this.gameOver();
+      } else {
+        this.noMatch();
+      }
+    }
+  };
+
+  // mantém as cartas viradas se forem iguais
+  match = () => {
+    this.removeClickEvent(this._firstCard);
+    this.removeClickEvent(this._secondCard);
+    this.reset();
+  };
+
+  gameOver = () => {
+    const flipped = this._cards.filter((card) => {
+      return card.classList.contains("flip");
+    });
+
+    if (flipped.length === this._cards.length) {
+      this.gameOverMessages();
+      this._modal.classList.add("active");
+    }
+  };
+
+  gameOverMessages = () => {
+    const modalElements = {
+      title: this._modal.querySelector("h2"),
+      message: this._modal.querySelector("p"),
+      record: this._modal.querySelector("span"),
+    };
+
+    const record = window.localStorage.getItem("record");
+    if (!record || this.userAttempts < record) {
+      window.localStorage.setItem("record", this.userAttempts);
+      modalElements.title.innerText = "Parabéns, você bateu o seu recorde!!";
+      modalElements.record.innerText = `Recorde: ${this.userAttempts}`;
+    } else {
+      modalElements.title.innerText = "Game Over, tente novamente!";
+      modalElements.record.innerText = `Recorde: ${record}`;
+    }
+
+    modalElements.message.innerText = `${this.userAttempts} tentativas`;
+  };
+
+  // desvira as cartas se não forem iguais
+  noMatch = () => {
+    setTimeout(() => {
+      this.addClickEvent(this._firstCard);
+      this.addClickEvent(this._secondCard);
+      this._firstCard.classList.remove("flip");
+      this._secondCard.classList.remove("flip");
+      this.reset();
+    }, 1200);
+  };
+
   restart = () => {
     this._cards.forEach((card) => {
-      card.classList.remove('flip');
+      card.classList.remove("flip");
       this.shuffle(card);
       this.addClickEvent(card);
     });
-    this._modal.classList.remove('active');
+    this._modal.classList.remove("active");
     this.userAttempts = 0;
-  }
+  };
 
   shuffle = (card) => {
     const randomNumber = Math.ceil(Math.random() * 15);
