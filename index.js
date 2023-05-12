45e82ad6a1180ad7584d31e3d4af915e02ce640d
GabriellBarbosa <bielbarbosa900@gmail.com>
2023-05-12 09:58:05 -0300

    replace arrow function with normal function

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import { MemoryGame } from './js/MemoryGame.js';
 import { debounce } from './js/debounce.js';
 
-// Tela de aviso: virar o celular
-  const turnPhoneElement = document.querySelector('.turn-phone-container');
-  if(window.innerWidth < 440) { 
+const turnPhoneElement = document.querySelector('.turn-phone-container');
+
+if (window.innerWidth < 440) { 
+  turnPhoneElement.classList.add('active');
+}
+
+window.addEventListener('resize', debounce(turnPhoneOnSmallScreen, 200));
+
+function turnPhoneOnSmallScreen() {
+  if (window.innerWidth > 440) {
+    turnPhoneElement.classList.remove('active');
+  } else {
     turnPhoneElement.classList.add('active');
   }
-  const handleScreenWidth = () => {
-    if(window.innerWidth > 440) {
-      turnPhoneElement.classList.remove('active');
-    } else {
-      turnPhoneElement.classList.add('active');
-    }
-  }
-  window.addEventListener('resize', debounce(handleScreenWidth, 200));
+}
+
 
 const cardsNodeList = document.querySelectorAll('.memory_card');
 const modalElement = document.querySelector('.modal-bg')
